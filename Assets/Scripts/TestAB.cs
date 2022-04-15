1a311c1bb8fcefeda21a70a6969c1338363e13eb
jom29 <42934171+jom29@users.noreply.github.com>
2022-04-15 18:03:43 +0800

    Initial Project Setup
    Project setup, to make sure that it is working on Target platform for Android.
    
    Unity Version: Unity 2019.4.38f1 (64-bit)
    Android Studio: 2021.1.1 Patch 3
    API Level 11.0 (R)
    Android SDK Platform Tools - 31.0.2
    JDK version: jdk-8u202-windows-x64
    Gradle bundled 211.7628.2.1.21111.8309675

---
+++ Assets/Scripts/TestAB.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestAB : MonoBehaviour
+{
+    public Text ui_gui;
+
+    public void PrintButton(string value)
+    {
+        ui_gui.text = value;
+    }
+
+    
+}
